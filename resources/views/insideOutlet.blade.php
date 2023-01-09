550cc8931330d675a0da04f025356ae3a0e1b863
Steven Tanugraha <stevenyanugraha@gmail.com>
2023-01-09 08:05:11 +0700

    Styling MenuSeller & InsideOutlet

--- resources/views/insideOutlet.blade.php
+++ resources/views/insideOutlet.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/inside-outlet.css') }}">
+@endsection
+
 @section('content')
     <div class="container">
         
@@ -8,30 +12,69 @@
         @endforeach --}}
 
         <div class="row justify-content-center">
-                <form action="/searchProductOutlet" method="get" style="margin-top: 20px">
+            <form action="/searchProduct" method="get" style="margin-top: 20px; width:100%" class="search">
+                <table>
+                    <td>
+                        <input type="text" class="form-control search-input" placeholder="Search..." name="query">
+                    </td>
 
-                        <table>
-                            <td>
-                                <input type="text" class="form-control" placeholder="Search..." name="query">
-                            </td>
+                    <td>
+                    <button type="submit" class="btn btn-success  search-btn" >
+                        Search
+                    </button>
+                    </td>
+                
+                </table>
+            </form>
 
-                            <td>
-                            <button type="submit" class="btn btn-success" >
-                                Search
-                            </button>
-                            </td>
-                        
-                        </table>
-                </form>
-            
-
-                <div class="outletContainer" style="display: grid; grid-template-columns: repeat(1, 1fr);margin-top: 20px">
+                {{-- <div class="outletContainer" style="display: grid; grid-template-columns: repeat(1, 1fr);margin-top: 20px"> --}}
 
                 @php
                     $totalPrice = 0;
                 @endphp
 
-                @forelse ($product as $pd) 
+                <table class="table menu-card" style="width: 377px;margin-top: 20px">
+
+                    @forelse ($product as $pd)
+                    
+                        <tr class="clickable" onclick="window.location='/menuDetailBuyer/{{$pd->id}}'">
+                            <td>
+                                    <img src="{{ Storage::url($pd->image) }}" style="width: 80.91px;height: 80.91px;border-radius: 10px" alt="Gambar" class="product-img">
+                            </td>
+                            <td>
+                                <span class="menu-name">
+                                    {{$pd->name}} <br>
+                                </span>
+                                <span class="menu-price">
+                                    Rp. {{$pd->price}}
+                                </span>
+                            </td>
+                            <td class="menu-stock">
+                                <span>Stock: <br></span>
+                                 {{$pd->stock}} pcs
+                            </td>
+                            {{-- <td class="delete-btn"> --}}
+                                {{-- <a href="/deleteMenu/{{$pd->id}}" > --}}
+                                    {{-- Delete --}}
+                                    {{-- <img src="{{ Storage::url("images\delete-icon.png") }}" alt="delete-icon">
+                                </a> --}}
+                            {{-- </td> --}}
+
+                        </tr> 
+                    
+                    @php
+                        // masih belum finish
+                        $totalPrice += 0;
+                    @endphp
+
+                    @empty
+                    <td id="datanotfound" style="text-align: center">There is no product yet</td>
+                    @endforelse
+
+                </table>
+
+
+                {{-- @forelse ($product as $pd) 
                     <a href="#" style="color: black">
                         <div style="display: grid; grid-template-columns: repeat(3, 1fr);width: 400px;margin-left: 40px">
                             <img src="{{ Storage::url($pd->image)}}" style="width: 84px;height: 84px;border-radius: 10px" alt="Img">
@@ -49,15 +92,15 @@
                 @empty
                     <p style="margin-top: 30px"> There is no product yet </p>
                 @endforelse
-                    
+                     --}}
                 
-                    <div style="display: flex;justify-content: space-evenly;align-items: center;margin: auto;margin-top: 40px;border: 1px black solid;border-radius: 10px;width: 349px;padding: 10px 0 10px 0">
-                        <b>Total: Rp {{$totalPrice}}</b>
+                    <div class="checkout-container">
+                        <p>Total: Rp {{$totalPrice}}</p>
                         <form action="#" method="POST">
-                            <button type="submit" class="btn btn-danger" style="border-radius: 10px">Checkout</button>
+                            <button type="submit" class="btn btn-danger" style="border-radius: 10px" id="checkout-btn">Checkout</button>
                         </form>
                     </div>
-                </div>
+                {{-- </div> --}}
         
         </div>
     </div>
--- resources/views/menuSeller.blade.php
+++ resources/views/menuSeller.blade.php
@@ -4,6 +4,10 @@
 <h4>Outlet Name</h4>
 @endsection --}}
 
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/menu-seller.css') }}">
+@endsection
+
 @section('content')
 <div class="container">
 
@@ -11,15 +15,14 @@
     
     <div class="row justify-content-center">
       
-            <form action="/searchProduct" method="get" style="margin-top: 20px">
-
+            <form action="/searchProduct" method="get" style="margin-top: 20px; width:100%" class="search">
                     <table>
                         <td>
-                            <input type="text" class="form-control" placeholder="Search..." name="query">
+                            <input type="text" class="form-control search-input" placeholder="Search..." name="query">
                         </td>
 
                         <td>
-                        <button type="submit" class="btn btn-success" >
+                        <button type="submit" class="btn btn-success  search-btn" >
                             Search
                         </button>
                         </td>
@@ -27,23 +30,33 @@
                     </table>
             </form>
 
-        <a href="/addNewProductSeller" style="margin-left: 130px;margin-top: 20px" class="btn btn-primary">Add New Product</a>
+        <a href="/addNewProductSeller" style="margin-left: 210px" class="btn btn-primary" id="new-product-btn">Add New Product</a>
 
-        <table class="table" style="width: 300px;margin-top: 20px">
+        <table class="table menu-card" style="width: 377px;margin-top: 20px">
 
                     @forelse ($product as $pd)
                      
-                        <tr>
-                            <td ><img src="{{ Storage::url($pd->image) }}" style="width: 80.91px;height: 80.91px;border-radius: 10px" alt="Gambar"></td>
-                            <td>{{$pd->name}} <br>
-                                Rp. {{$pd->price}}
+                        <tr class="clickable" onclick="window.location='/menuDetailSeller/{{$pd->id}}'">
+                            <td>
+                                    <img src="{{ Storage::url($pd->image) }}" style="width: 80.91px;height: 80.91px;border-radius: 10px" alt="Gambar" class="product-img">
                             </td>
                             <td>
+                                <span class="menu-name">
+                                    {{$pd->name}} <br>
+                                </span>
+                                <span class="menu-price">
+                                    Rp. {{$pd->price}}
+                                </span>
+                            </td>
+                            <td class="menu-stock">
                                {{$pd->stock}} pcs
                             </td>
-                            <td>
-                                <a href="/menuDetailSeller/{{$pd->id}}" class="btn btn-primary">View</a>
-                                <a href="/deleteMenu/{{$pd->id}}" class="btn btn-danger">Delete</a></td>
+                            <td class="delete-btn">
+                                <a href="/deleteMenu/{{$pd->id}}" >
+                                    {{-- Delete --}}
+                                    <img src="{{ Storage::url("images\delete-icon.png") }}" alt="delete-icon">
+                                </a>
+                            </td>
 
                         </tr> 
                     
