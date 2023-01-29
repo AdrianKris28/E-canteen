c1be99ae655f9f91fc2b09ac37c76261f93e52ed
Steven Tanugraha <stevenyanugraha@gmail.com>
2023-01-29 19:48:08 +0700

    update default profile image

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,6 +70,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'phone' => $data['phone'],
+            'image' => "images/profile-image.jpg",
         ]);
     }
 }
